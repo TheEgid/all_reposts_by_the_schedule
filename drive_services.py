175265ec53e3fe3cd966086ca60a04993d9a594e
TheEgid <40724661+TheEgid@users.noreply.github.com>
2019-03-12 10:58:09 +0300

    Update drive_services.py

--- drive_services.py
+++ drive_services.py
@@ -1,2 +1,46 @@
-def print_table(table_data, title):
-   pass
+from pydrive.auth import GoogleAuth
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from oauth2client.client import GoogleCredentials
+
+
+def get_file(file_id, title):
+    file_id = 'abc'
+	file_access_token = '34244324324'
+	scope = 'https://www.googleapis.com/auth/drive.readonly'
+	path_to_private_key_file = '#'
+	service_account_name = 'j35@developer.gserviceaccount.com'
+
+	drive_client = GoogleDriveClient(
+               scope = scope,
+               private_key = open(path_to_private_key_file).read(),
+               service_account_name = service_account_name)
+
+	file = drive_client.get_drive_file(file_id, file_access_token)
+
+   
+#http://qaru.site/questions/165781/automating-pydrive-verification-process
+def drive_autorization():
+    file_id = 'abc'
+	gauth = GoogleAuth()
+	# Try to load saved client credentials
+	gauth.LoadCredentialsFile("mycreds.txt")
+	if gauth.credentials is None:
+		gauth.LocalWebserverAuth()
+	elif gauth.access_token_expired:
+		gauth.Refresh()
+	else:
+		gauth.Authorize()
+	gauth.SaveCredentialsFile("mycreds.txt")
+
+	
+	drive = GoogleDrive(gauth)
+	textfile = drive.CreateFile()
+	textfile.SetContentFile('eng.txt')
+	textfile.Upload()
+	print (textfile)
+	drive.CreateFile({'id':textfile['id']}).GetContentFile('eng-dl.txt')
+   
+if __name__ == '__main__':
+   drive_autorization()
+   
