13a5e749d5d9f1dd058fa758fccb16ed98574ddd
TheEgid <40724661+TheEgid@users.noreply.github.com>
2019-03-13 17:09:03 +0300

    Update new 1.py

--- new 1.py
+++ new 1.py
@@ -1,7 +1,9 @@
+
 schedule_spreadsheet = [['нет', 'да', 'нет', 'вторник', 10, '=HYPERLINK("https://drive.google.com/open?id=1Wthk5k2ucVXpsa_J2F9TTflA2ztjcarLu81Rtb05ps8";"Лапы")', '=HYPERLINK("https://drive.google.com/open?id=1zljULNgX1h1sS9x4iWLaDZQEVEMNf5PH";"Лапы")', 'да'], ['да', 'да', 'да', 'суббота', 15, '=HYPERLINK("https://drive.google.com/open?id=1kBFAOegBzmoC7I9ufv8rmFFqcSGqwA90ClE8hJ9cGFE";"Обоняние")', '=HYPERLINK("https://drive.google.com/open?id=1LEltAYogLBboyXgdyVH_1rW6Flr3zzjm";"Обоняние")', 'нет'], ['нет', 'да', 'да', 'четверг', 19, '=HYPERLINK("https://drive.google.com/open?id=1mS2RZO-TvhXZXqWQKx5AiWtCF-gkJHGAqtk59pAsHqA";"Усы")', '=HYPERLINK("https://drive.google.com/open?id=12V8spqPdepdMVflmcIoXMNb2SHlc7zdS";"Усы")', 'нет'], ['нет', 'нет', 'да', 'вторник', 19, '=HYPERLINK("https://drive.google.com/open?id=18uujc6MSE-byyAHKQHo_E9mA3Hdhsfu3kPXd9A3NcaQ";"Одиночество")', '=HYPERLINK("https://drive.google.com/open?id=1gsNyLmQvCmTqWV9WRw7exTnK31BPg-4i";"Одиночество")', 'да'], ['да', 'нет', 'нет', 'суббота', 15, '=HYPERLINK("https://drive.google.com/open?id=1iRiqwfofCqNRrsVVP0Sv6Tk5CkWfVztk2hJRzySNQlo";"Темнота")', '=HYPERLINK("https://drive.google.com/open?id=1eIzluX3xvZRiALjX-PQZeHNrHHHMjRJ1";"Темнота")', 'да'], ['да', 'да', 'нет', 'понедельник', 13, '=HYPERLINK("https://drive.google.com/open?id=1-gnx8kZnS8FZUd4DaoShb4RPD7D3n8wQxhVMRmUN7BI";"Про кошек")', '=HYPERLINK("https://drive.google.com/open?id=1yKE3DsV3ya0YzTpWDKH4sUxPo_4BMauB";"Кошки")', 'нет'], ['нет', 'нет', 'да', 'четверг', 12, '=HYPERLINK("https://drive.google.com/open?id=1Wthk5k2ucVXpsa_J2F9TTflA2ztjcarLu81Rtb05ps8";"Лапы")', '=HYPERLINK("https://drive.google.com/open?id=1zljULNgX1h1sS9x4iWLaDZQEVEMNf5PH";"Лапы")', 'да'], ['да', 'нет', 'нет', 'вторник', 16, '=HYPERLINK("https://drive.google.com/open?id=1mS2RZO-TvhXZXqWQKx5AiWtCF-gkJHGAqtk59pAsHqA";"Усы")', '=HYPERLINK("https://drive.google.com/open?id=12V8spqPdepdMVflmcIoXMNb2SHlc7zdS";"Усы")', 'нет'], ['нет', 'да', 'нет', 'суббота', 14, '=HYPERLINK("https://drive.google.com/open?id=18uujc6MSE-byyAHKQHo_E9mA3Hdhsfu3kPXd9A3NcaQ";"Одиночество")', '=HYPERLINK("https://drive.google.com/open?id=1gsNyLmQvCmTqWV9WRw7exTnK31BPg-4i";"Одиночество")', 'нет'], ['да', 'нет', 'нет', 'понедельник', 16, '=HYPERLINK("https://drive.google.com/open?id=18uujc6MSE-byyAHKQHo_E9mA3Hdhsfu3kPXd9A3NcaQ";"Одиночество")', '=HYPERLINK("https://drive.google.com/open?id=1gsNyLmQvCmTqWV9WRw7exTnK31BPg-4i";"Одиночество")', 'да'], ['нет', 'да', 'да', 'четверг', 21, '=HYPERLINK("https://drive.google.com/open?id=1iRiqwfofCqNRrsVVP0Sv6Tk5CkWfVztk2hJRzySNQlo";"Темнота")', '=HYPERLINK("https://drive.google.com/open?id=1eIzluX3xvZRiALjX-PQZeHNrHHHMjRJ1";"Темнота")', 'да'], ['нет', 'нет', 'да', 'вторник', 10, '=HYPERLINK("https://drive.google.com/open?id=1-gnx8kZnS8FZUd4DaoShb4RPD7D3n8wQxhVMRmUN7BI";"Про кошек")', '=HYPERLINK("https://drive.google.com/open?id=1yKE3DsV3ya0YzTpWDKH4sUxPo_4BMauB";"Кошки")', 'нет']]
 
 #schedule_row = ['нет', 'нет', 'да', 'вторник', 10, '=HYPERLINK("https://drive.google.com/open?id=1-gnx8kZnS8FZUd4DaoShb4RPD7D3n8wQxhVMRmUN7BI";"Про кошек")', '=HYPERLINK("https://drive.google.com/open?id=1yKE3DsV3ya0YzTpWDKH4sUxPo_4BMauB";"Кошки")', 'нет']
 
+import datetime
 from urllib.parse import parse_qsl
 from urlextract import URLExtract
 
@@ -40,11 +42,22 @@ new_schedule_spreadsheet = choice_schedule_row_to_publish(schedule_spreadsheet, 
 for row in new_schedule_spreadsheet:
 	print(row)
 	
-
-
 ###
 
 s1 = ['нет', 'нет', 'да', 'вторник', 10, '1-gnx8kZnS8FZUd4DaoShb4RPD7D3n8wQxhVMRmUN7BI', '1yKE3DsV3ya0YzTpWDKH4sUxPo_4BMauB', 'нет']	
+
+def calculate_publish_moment(moment):
+	all_days = {'понедельник':0, 'вторник':1, 'среда':2, 'четверг':3, 'пятница':4, 'суббота':6, 'воскресенье':7}
+	publish_day, publish_time = moment
+	now = datetime.datetime.now()
+	future_day = datetime.date(now.year, now.month, now.day)
+	while future_day.weekday() != all_days[publish_day.lower()]: 
+		future_day += datetime.timedelta(days=1)
+	publish_moment = datetime.datetime.combine(future_day, datetime.time(publish_time))
+	if now >= publish_moment:
+		return None
+	else:
+		return publish_moment
 	
 def procss(schedule_row):
 	if len(schedule_row) != 8:
@@ -53,12 +66,6 @@ def procss(schedule_row):
 	moment = [publish_day, publish_time]
 	content = [txt_id, img_id]
 	flags = [flag_vk, flag_tg, flag_fb]
-	return moment, content, flags
+	return (calculate_publish_moment(moment), content, flags)
 
 print(procss(s1))		
-		
-mm = ['вторник', 10]
-
-
-days = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье']
-print (days[datetime.date.today().weekday()])
