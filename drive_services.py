491adc1b11f273c89a42c92aaf35874140068d26
TheEgid <40724661+TheEgid@users.noreply.github.com>
2019-03-15 16:24:15 +0300

    Update drive_services.py

--- drive_services.py
+++ drive_services.py
@@ -1,33 +1,28 @@
-
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
 #https://github.com/MSF-Jarvis/pydrive-client/blob/master/main.py   
 #http://qaru.site/questions/165781/automating-pydrive-verification-process
-def drive_autorization():
-    gauth = GoogleAuth()
-    gauth.LoadCredentialsFile("mycreds.txt")
-    if gauth.credentials is None:
-        gauth.LocalWebserverAuth()
-    elif gauth.access_token_expired:
-        gauth.Refresh()
-    else:
-        gauth.Authorize()
-    gauth.SaveCredentialsFile("mycreds.txt")
 
-    drive = GoogleDrive(gauth)
 
-    file_id = '1yKE3DsV3ya0YzTpWDKH4sUxPo_4BMauB'
-    file = drive.CreateFile({'id': file_id})
-    file.FetchMetadata()
-    print(file.metadata['mimeType'])
-    print(file.metadata['title'])
-    print(file.metadata['webContentLink'])
 
-    # print(
-    #     'Downloading file %s from Google Drive' % file3['title'])
+def get_file_metadata_from_gdrive(file_id, credential_file="mycreds.txt"):
+  gauth = GoogleAuth()
+  gauth.LoadCredentialsFile(credential_file)
+  if gauth.credentials is None:
+    gauth.LocalWebserverAuth()
+  elif gauth.access_token_expired:
+    gauth.Refresh()
+  else:
+    gauth.Authorize()
+  gauth.SaveCredentialsFile(credential_file)
+  drive = GoogleDrive(gauth)
+  myfile = drive.CreateFile({'id': file_id})
+  myfile.FetchMetadata()
+  file_metadata = {file_type: file.metadata['mimeType'], file_tittle: file.metadata['title'], file_link: file.metadata['webContentLink']}
+  return file_metadata
 
 
 if __name__ == '__main__':
-    drive_autorization()
-
+  file_id = '1yKE3DsV3ya0YzTpWDKH4sUxPo_4BMauB'
+  print(get_file_metadata_from_gdrive(file_id))
