66996b6aa0c0f00cebe68b14fb97db7c586ff04a
TheEgid <40724661+TheEgid@users.noreply.github.com>
2019-03-16 12:01:31 +0300

    r

--- main.py
+++ main.py
@@ -1,56 +1,21 @@
 from __future__ import print_function
 import pickle
 import os.path
+
 import datetime
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
-from urllib.parse import parse_qsl
-from urlextract import URLExtract
-from drive_services import get_file_metadata_from_gdrive
 
-spreadsheet = [['нет', 'да', 'нет', 'пятница', 22,
-                '=HYPERLINK("https://drive.google.com/open?id=1Wthk5k2ucVXpsa_J2F9TTflA2ztjcarLu81Rtb05ps8";"Лапы")',
-                '=HYPERLINK("https://drive.google.com/open?id=1zljULNgX1h1sS9x4iWLaDZQEVEMNf5PH";"Лапы")',
-                'нет'], ['да', 'да', 'да', 'суббота', 15,
-                         '=HYPERLINK("https://drive.google.com/open?id=1kBFAOegBzmoC7I9ufv8rmFFqcSGqwA90ClE8hJ9cGFE";"Обоняние")',
-                         '=HYPERLINK("https://drive.google.com/open?id=1LEltAYogLBboyXgdyVH_1rW6Flr3zzjm";"Обоняние")',
-                         'нет'], ['нет', 'да', 'да', 'четверг', 19,
-                                  '=HYPERLINK("https://drive.google.com/open?id=1mS2RZO-TvhXZXqWQKx5AiWtCF-gkJHGAqtk59pAsHqA";"Усы")',
-                                  '=HYPERLINK("https://drive.google.com/open?id=12V8spqPdepdMVflmcIoXMNb2SHlc7zdS";"Усы")',
-                                  'нет'],
-               ['нет', 'нет', 'да', 'вторник', 19,
-                '=HYPERLINK("https://drive.google.com/open?id=18uujc6MSE-byyAHKQHo_E9mA3Hdhsfu3kPXd9A3NcaQ";"Одиночество")',
-                '=HYPERLINK("https://drive.google.com/open?id=1gsNyLmQvCmTqWV9WRw7exTnK31BPg-4i";"Одиночество")',
-                'да'], ['да', 'нет', 'нет', 'суббота', 15,
-                        '=HYPERLINK("https://drive.google.com/open?id=1iRiqwfofCqNRrsVVP0Sv6Tk5CkWfVztk2hJRzySNQlo";"Темнота")',
-                        '=HYPERLINK("https://drive.google.com/open?id=1eIzluX3xvZRiALjX-PQZeHNrHHHMjRJ1";"Темнота")',
-                        'да'], ['да', 'да', 'нет', 'понедельник', 13,
-                                '=HYPERLINK("https://drive.google.com/open?id=1-gnx8kZnS8FZUd4DaoShb4RPD7D3n8wQxhVMRmUN7BI";"Про кошек")',
-                                '=HYPERLINK("https://drive.google.com/open?id=1yKE3DsV3ya0YzTpWDKH4sUxPo_4BMauB";"Кошки")',
-                                'нет'], ['нет', 'нет', 'да', 'четверг', 12,
-                                         '=HYPERLINK("https://drive.google.com/open?id=1Wthk5k2ucVXpsa_J2F9TTflA2ztjcarLu81Rtb05ps8";"Лапы")',
-                                         '=HYPERLINK("https://drive.google.com/open?id=1zljULNgX1h1sS9x4iWLaDZQEVEMNf5PH";"Лапы")',
-                                         'да'],
-               ['да', 'нет', 'нет', 'пятница', 10,
-                '=HYPERLINK("https://drive.google.com/open?id=1mS2RZO-TvhXZXqWQKx5AiWtCF-gkJHGAqtk59pAsHqA";"Усы")',
-                '=HYPERLINK("https://drive.google.com/open?id=12V8spqPdepdMVflmcIoXMNb2SHlc7zdS";"Усы")',
-                'нет'], ['нет', 'да', 'нет', 'суббота', 14,
-                         '=HYPERLINK("https://drive.google.com/open?id=18uujc6MSE-byyAHKQHo_E9mA3Hdhsfu3kPXd9A3NcaQ";"Одиночество")',
-                         '=HYPERLINK("https://drive.google.com/open?id=1gsNyLmQvCmTqWV9WRw7exTnK31BPg-4i";"Одиночество")',
-                         'нет'], ['да', 'нет', 'нет', 'понедельник', 16,
-                                  '=HYPERLINK("https://drive.google.com/open?id=18uujc6MSE-byyAHKQHo_E9mA3Hdhsfu3kPXd9A3NcaQ";"Одиночество")',
-                                  '=HYPERLINK("https://drive.google.com/open?id=1gsNyLmQvCmTqWV9WRw7exTnK31BPg-4i";"Одиночество")',
-                                  'да'], ['нет', 'да', 'да', 'четверг', 21,
-                                          '=HYPERLINK("https://drive.google.com/open?id=1iRiqwfofCqNRrsVVP0Sv6Tk5CkWfVztk2hJRzySNQlo";"Темнота")',
-                                          '=HYPERLINK("https://drive.google.com/open?id=1eIzluX3xvZRiALjX-PQZeHNrHHHMjRJ1";"Темнота")',
-                                          'да'],
-               ['нет', 'нет', 'да', 'вторник', 10,
-                '=HYPERLINK("https://drive.google.com/open?id=1-gnx8kZnS8FZUd4DaoShb4RPD7D3n8wQxhVMRmUN7BI";"Про кошек")',
-                '=HYPERLINK("https://drive.google.com/open?id=1yKE3DsV3ya0YzTpWDKH4sUxPo_4BMauB";"Кошки")',
-                'нет']]
+from services import get_file_metadata_from_gdrive
+from services import save_file
+from services import extract_google_spreadsheet_id
+from services import extract_file_id
 
-def get_spreadsheet(spreadsheet_id, range_name):
+
+
+def get_spreadsheet(link, range_name):
+    spreadsheet_id = extract_google_spreadsheet_id(link)
     creds = None
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
@@ -77,14 +42,6 @@ def get_spreadsheet(spreadsheet_id, range_name):
         return None
 
 
-def extract_file_id(text):
-    text= str(text)
-    try:
-        _text = URLExtract().find_urls(text)[0]
-        return parse_qsl(_text)[0][1]
-    except IndexError:
-        return None
-
 
 def check_publish_moment(publish_day, publish_time):
     all_days = {'понедельник':0, 'вторник':1, 'среда':2, 'четверг':3,
@@ -101,7 +58,7 @@ def check_publish_moment(publish_day, publish_time):
         print('start')
         return True
     else:
-        print('рано! '+str(publish_moment)+' '+str(now))
+        #print('рано! '+str(publish_moment)+' '+str(now))
         return False
 
 
@@ -111,24 +68,21 @@ def check_spreadsheet(schedule_spreadsheet, non ="нет"):
             raise ValueError('Incorrect! Check the schedule spreadsheet!')
         flag_vk, flag_tg, flag_fb, publish_day, publish_time, txt_id, img_id, non_published_flag = schedule_row
         if non_published_flag.lower() != non:
-            print('уже',non_published_flag)
+            #print('уже',non_published_flag)
             pass
         else:
-            flags = {'vk':flag_vk, 'tg': flag_tg, 'fb': flag_fb}
-            content = [x if extract_file_id(x) is None else extract_file_id(x) for x in [txt_id, img_id]]
+            flags = {'vk': flag_vk, 'tg': flag_tg, 'fb': flag_fb}
+            all_content = [x if extract_file_id(x) is None else extract_file_id(x) for x in [txt_id, img_id]]
             publish_moment = check_publish_moment(publish_day, publish_time)
             if publish_moment:
-                print('gogogo')
-                print(content)
-                print(get_file_metadata_from_gdrive(content[0]))
-                print(flags, flags['vk'])
+                content = list(map(get_file_metadata_from_gdrive, all_content))
+                s = [save_file(x['file_link'], x['file_title']) for x in content]
+
 
 
 if __name__ == '__main__':
-    SPREADSHEET_ID = '17r4QRW_m0clut772bRnUL-U1-JiazImiZMm43SkgS9Q'
+    spreadsheet_link = 'https://docs.google.com/spreadsheets/d/10kRjz6TXNHtlEWaOTgMB0RZ_eLz8I6DZwEo78d9R9Bs/edit#gid=0'
     RANGE_NAME = 'Лист1!A3:H100000'
-    #spreadsheet = get_spreadsheet(SPREADSHEET_ID, RANGE_NAME)
-
-
+    spreadsheet = get_spreadsheet(spreadsheet_link, RANGE_NAME)
     check_spreadsheet(spreadsheet)
 
--- services.py
+++ services.py
@@ -1,13 +1,33 @@
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
+from urllib.parse import parse_qsl
+from urlextract import URLExtract
 import requests
 import os
+import re
+
+
+def extract_google_spreadsheet_id(_string):
+    regex = r'[^d/][A-Za-z0-9_]{20,}'
+    spreadsheet_id = re.findall(regex, _string)
+    if not spreadsheet_id:
+        return None
+    else:
+        return spreadsheet_id[0]
+
+
+def extract_file_id(text):
+    text= str(text)
+    try:
+        _text = URLExtract().find_urls(text)[0]
+        return parse_qsl(_text)[0][1]
+    except IndexError:
+        return None
+
 
 # https://github.com/MSF-Jarvis/pydrive-client/blob/master/main.py
 # http://qaru.site/questions/165781/automating-pydrive-verification-process
-
-
-def save_file(url, filename, dir_name='images/'):
+def save_file(url, filename, dir_name='content_folder/'):
     filepath = dir_name+filename
     if not os.path.exists(dir_name):
         os.makedirs(dir_name)
@@ -50,15 +70,16 @@ def get_file_metadata_from_gdrive(file_id, credential_file="mycreds.txt"):
 
 
 
+
 if __name__ == '__main__':
     #file_id = '1zljULNgX1h1sS9x4iWLaDZQEVEMNf5PH'
 
-    r = ['1Wthk5k2ucVXpsa_J2F9TTflA2ztjcarLu81Rtb05ps8',
-         '1zljULNgX1h1sS9x4iWLaDZQEVEMNf5PH']
+    r = ['18uujc6MSE-byyAHKQHo_E9mA3Hdhsfu3kPXd9A3NcaQ', '1gsNyLmQvCmTqWV9WRw7exTnK31BPg-4i']
 
     for el in r:
         print(get_file_metadata_from_gdrive(el))
         data = get_file_metadata_from_gdrive(el)
 
         save_file(data['file_link'], data['file_title'])
+
 
